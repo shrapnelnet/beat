9659789289e4089e6a66aeee90cef5b6cb51cbd2
vulonkaaz <7442677+vulonkaaz@users.noreply.github.com>
2024-08-09 17:09:54 +0200

    making code "prettier" to be pushed upstream

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <time.h>
 
+// number of seconds in an hour, a day and a swatch beat
+#define ONE_HOUR 3600
+#define ONE_DAY 86400
+#define ONE_BEAT 86.4
+
 int main(void) {
     time_t CURRENT_TIME = time(NULL);
-    // +3600 cause swatch time is UTC + 1, % 86400 to have number of sec since midnight
-    int beat = ((CURRENT_TIME + 3600) % 86400 ) / 86.4;
+    const int oneHour = 3600;
+    const int oneDay = 86400;
+    int beat = ((CURRENT_TIME + ONE_HOUR) % ONE_DAY ) / ONE_BEAT;
     printf("@%d\n", beat);
     return 0;
 }
